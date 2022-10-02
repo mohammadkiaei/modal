acece8d93689b915d3ff97a79dbf26ddfbc250b0
mohammadkiaei <s.kiaei.s.m.m.k1380@gmail.com>
2022-10-02 12:06:48 +0330

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const openBtn = document.querySelector('.open');
+const modalContainer = document.querySelector('.modal-container');
+const closeBtn = document.querySelector('.modal-btn');
+
+openBtn.addEventListener('click', function () {
+   modalContainer.classList.add('show');
+});
+
+closeBtn.addEventListener('click', function () {
+   modalContainer.classList.remove('show');
+});
+
+// Developed by SeyedMohammadMahdi kiaei
